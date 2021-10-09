8588e2c437fde6d85dfe0eb65df8fbe0bdd094fa
Elias Schneider <login@eliasschneider.com>
2021-10-09 12:20:20 +0200

    updated github action

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -4,6 +4,6 @@
 
 <footer id="sticky-footer" class="flex-shrink-0 py-4">
 	<div class="container text-center">
-		<small>Copyright {year} &copy; Elias Schneider</small>
+		<small>&copy; {year} Elias Schneider</small>
 	</div>
 </footer>
