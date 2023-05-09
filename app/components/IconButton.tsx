654fb456f939e31b1942af4f27f31e63ddc93159
Elias Schneider <login@eliasschneider.com>
2023-05-09 16:58:59 +0200

    fix: make icon button border clickable

--- app/components/IconButton.tsx
+++ app/components/IconButton.tsx
@@ -9,19 +9,17 @@ export default function IconButton({
   className?: string;
 } & React.AnchorHTMLAttributes<HTMLAnchorElement>) {
   return (
-    <div className={"grid items-start justify-center gap-8 " + className}>
+    <Link
+      href={linkProps.href!}
+      {...linkProps}
+      className={"grid items-start justify-center gap-8 " + className}
+    >
       <div className="group relative">
         <div className="animate-tilt absolute -inset-[0.1px] rounded-lg bg-gradient-to-r from-pink-600 to-purple-600 opacity-75 blur transition duration-100 group-hover:opacity-100 group-hover:duration-200"></div>
-        <div className="relative rounded-lg bg-black p-3 leading-3">
-          {linkProps.href ? (
-            <Link className="text-white" href={linkProps.href} {...linkProps}>
-              {children}
-            </Link>
-          ) : (
-            <button>{children}</button>
-          )}
+        <div className="relative rounded-lg bg-black p-3 leading-3 text-white">
+          {children}
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
